3564f132b8667a40d077ca170afb080256c786fd
raahilaalam <156442056+raahilaalam@users.noreply.github.com>
2025-01-08 13:54:31 -0500

    App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Custom React App</h1>
+      <p>This is a custom React app created with a modified index.html file.</p>
+    </div>
+  );
+}
+
+export default App;
